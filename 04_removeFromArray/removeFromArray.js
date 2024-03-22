b007f9e08956b8162748ef8eab39fce44ec69d12
taachii <taachii21@outlook.com>
2024-03-22 09:59:29 +0100

    Do ex. 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,21 @@
-const removeFromArray = function() {
+/**
+ * 
+ * @param {number[]} arr 
+ * @param {number[]} nums
+ */
 
+const removeFromArray = function(arr, ...nums) {
+  // const newArr = [];
+
+  // arr.forEach((num) => {
+  //   if(!nums.includes(num)) {
+  //     newArr.push(num);
+  //   }
+  // });
+
+  // return newArr;
+  
+  return arr.filter(num => !nums.includes(num));
 };
 
 // Do not edit below this line
