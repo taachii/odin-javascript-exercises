b51bd95d8e17d9e296a199ead9b090cfd84ba622
taachii <taachii21@outlook.com>
2024-03-22 10:10:43 +0100

    Do ex. 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+/**
+ * 
+ * @param {number} x 
+ * @param {number} y 
+ */
 
+const sumAll = function(x, y) {
+
+  if(x < 0 || y < 0) return 'ERROR';
+  if( !Number.isInteger(x) || !Number.isInteger(y)) return 'ERROR'
+
+  const start = Math.min(x,y);
+  const end = Math.max(x,y);
+  
+  let sum = 0;
+  for(let i = start; i <= end; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
