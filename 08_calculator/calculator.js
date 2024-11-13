16306737e8c15ac1159640d736461621a9e1cebf
taachii <taachii21@outlook.com>
2024-11-13 08:59:25 +0100

    ex 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+  if(!nums.length) return 0;
+	return nums.reduce((sum, num) => sum + num);
 };
 
-const multiply = function() {
-
+const multiply = function(nums) {
+  return nums.reduce((product, num) => product * num);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	if(a === 0) return 1;
+
+  let result = 1;
+  for(let i = 2; i <= a; i++) {
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
