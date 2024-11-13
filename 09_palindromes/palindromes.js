f66fd8eb8febccd6011462d2878d9ff0f1c5196b
taachii <taachii21@outlook.com>
2024-11-13 09:40:37 +0100

    ex 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
 
+const palindromes = function (str) {
+  let punctuationless = str.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+  let finalString = punctuationless.replace(/\s+/g,"").toLowerCase();
+  let reversedString = finalString.split("").reverse().join("");
+
+  console.log(finalString);
+  console.log(reversedString);
+  return finalString === reversedString;
 };
 
 // Do not edit below this line
